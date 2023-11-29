3545937bf204fc07f72cd4b23362d0fe281a4883
le402n11\uer <410381276@m365.fju.edu.tw>
2023-11-29 18:01:44 +0800

    畫圖

--- 231129.R
+++ 231129.R
@@ -1,2 +1,4 @@
 dat = read.csv("Example data.csv", header = TRUE)
 head(dat)
+
+boxplot(dat[,"eGFR"] ~ dat[,"Disease"], col = c("blue", "red"), ylab = "eGFR", xlab = "Disease", main = "eGFR value by Disease status", lwd = 1.5)
